524327e1d1a61e54ae426fce608ddde32f8c008f
Santiago Clavijo <santiclav@hotmail.es>
2024-09-23 17:34:28 -0500

    sube archivos de configuracion

---
+++ backend/config/main-local.php
@@ -0,0 +1,45 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'mWBKAV-WGzdJnkfapr1YwJi6oEbzW_BP',
+            'baseUrl' => '/ligas/administrator',
+            'csrfParam' => '_csrf-backend',
+            'enableCsrfValidation' => true,
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'baseUrl' => '/ligas/administrator',
+            'rules' => [
+                '<controller>/<action>/<id:\d+>' => '<controller>/<action>',
+            ],
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => \yii\debug\Module::class,
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => \yii\gii\Module::class,
+        'generators' => [ // here
+            'crud' => [ // generator name
+                'class' => 'yii\gii\generators\crud\Generator', // generator class
+                'templates' => [ // setting for our templates
+                    'yii2-adminlte3' => '@vendor/hail812/yii2-adminlte3/src/gii/generators/crud/default' // template name => path to template
+                ]
+            ]
+        ]
+    ];
+}
+
+return $config;
